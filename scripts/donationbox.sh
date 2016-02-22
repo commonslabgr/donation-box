fec3e56308b1c7a673813595ffc79b77f59b0ab9
dkoukoul <dimitris.koukoulakis@gmail.com>
2016-02-22 19:57:03 +0200

    Update start script

--- scripts/donationbox.sh
+++ scripts/donationbox.sh
@@ -7,7 +7,8 @@ id=`xinput -list | grep Edamak | sed -n 's/.*\(id=\)//p' | sed -n 's/\t.*//p'`
 /usr/bin/xset -dpms
 /usr/bin/xset s noblank
 /usr/bin/chromium-browser http://donationbox/ --kiosk --incognito --touch-devices=$id
-/usr/bin/xinput set-int-prop "Edamak LG TS2009-F-USB" "Calibration" 32 27 2027 39 1977
+/usr/bin/xinput set-int-prop "Edamak LG TS2009F-USB" "Evdev Axis Calibration" 32 -73 2051 -13 2073
+/usr/bin/xinput set-int-prop "Edamak LG TS2009F-USB" "Evdev Axes Swap" 8 0
 sleep 30
 #Turn on monitor
 ./monitorOn.sh
